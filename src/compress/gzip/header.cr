1822f94448463cb5d8a19d6a819d8e24b9afd6fd
kojix2 <2xijok@gmail.com>
2024-07-05 13:22:12 +0900

    Revert "Fix Compress::Gzip error for gzip files with extra field"
    This reverts commit 073453241e6a6c9b04530467e3fdf4a565735390.

--- src/compress/gzip/header.cr
+++ src/compress/gzip/header.cr
@@ -41,7 +41,7 @@ class Compress::Gzip::Header
     @os = header[9]
 
     if flg.extra?
-      xlen = io.read_bytes(UInt16, IO::ByteFormat::LittleEndian)
+      xlen = io.read_byte.not_nil!
       @extra = Bytes.new(xlen)
       io.read_fully(@extra)
     end
@@ -86,7 +86,7 @@ class Compress::Gzip::Header
     io.write_byte os
 
     unless @extra.empty?
-      io.write_bytes(@extra.size.to_u16, IO::ByteFormat::LittleEndian)
+      io.write_byte @extra.size.to_u8
       io.write(@extra)
     end
 
