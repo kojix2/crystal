kojix2/crystal

commit 9faf49b5e0c8cabb22ec3ee3983cd4a582d0d3e2

Parse parens in type grammar into `Union#parens` (#16744)
The parser previously skipped parenthesis in type grammar expressions. They were considered a non-essential syntactical variation. The _effects_ for grouping expressions still registered, so the semantic result was complete.

Capturing the full syntactic expressiveness is useful for the formatter and other tooling that operates on syntax trees and needs to be aware of parenthesis semantics. This information is critical for formatter enhancements for rewriting parenthesis.

In the normal language, parenthesis map to `Expressions` nodes, which makes a clear assignment possible.

We transfer that mechanism to the type grammar and introduce a similar node for parenthesis. A `Union` is the ideal representation for that.

Since it's only needed for the syntax tree, we can clean up these extra nodes afterwards.

As a first effect, we can simplify the convoluted formatter code for dealing with parenthesis immensely. It's so much cleaner.

And it opens the door to make enhancements such as #11966 and #16728 relatively trivial to implement.

Co-authored-by: Sijawusz Pur Rahnama <sija@sija.pl>

src/compiler/crystal/tools/formatter.cr
@@ -129,13 +129,6 @@ module Crystal
       @exp_needs_indent = true
       @inside_def = 0
 
-      # When we parse a type, parentheses information is not stored in ASTs, unlike
-      # for an Expressions node. So when we are printing a type (Path, ProcNotation, Union, etc.)
-      # we increment this when we find a '(', and decrement it when we find ')', but
-      # only if `paren_count > 0`: it might be the case of `def foo(x : A)`, but we don't
-      # want to print that last ')' when printing the type A.
-      @paren_count = 0
-
       # This stores the column number (if any) of each comment in every line
       @when_infos = [] of AlignInfo
       @hash_infos = [] of AlignInfo
@@ -1058,29 +1051,7 @@ module Crystal
       false
     end
 
-    def check_open_paren
-      if @token.type.op_lparen?
-        while @token.type.op_lparen?
-          write "("
-          next_token_skip_space
-          @paren_count += 1
-        end
-        true
-      else
-        false
-      end
-    end
-
-    def check_close_paren
-      while @token.type.op_rparen? && @paren_count > 0
-        @paren_count -= 1
-        write_token :OP_RPAREN
-      end
-    end
-
     def visit(node : Path)
-      check_open_paren
-
       # Sometimes the :: is not present because the parser generates ::Nil, for example
       if node.global? && @token.type.op_colon_colon?
         write "::"
@@ -1099,14 +1070,10 @@ module Crystal
         end
       end
 
-      check_close_paren
-
       false
     end
 
     def visit(node : Generic)
-      check_open_paren
-
       name = node.name.as(Path)
       if name.global? && @token.type.op_colon_colon?
         write "::"
@@ -1207,17 +1174,12 @@ module Crystal
       accept name
       skip_space_or_newline
 
-      # Given that generic type arguments are always inside parentheses
-      # we can start counting them from 0 inside them.
-      old_paren_count = @paren_count
-      @paren_count = 0
-
       if named_args = node.named_args
         write_token :OP_LPAREN
         skip_space
         has_newlines, _, _ = format_named_args([] of ASTNode, named_args, @indent + 2)
         # `format_named_args` doesn't skip trailing comma
-        if @paren_count == 0 && @token.type.op_comma?
+        if @token.type.op_comma?
           next_token_skip_space_or_newline
           if has_newlines
             write ","
@@ -1225,28 +1187,23 @@ module Crystal
             write_indent
           end
         end
-        skip_space_or_newline if @paren_count == 0
+        skip_space_or_newline
         write_token :OP_RPAREN
       else
         format_literal_elements(node.type_vars, :OP_LPAREN, :OP_RPAREN)
       end
 
-      # Restore the old parentheses count
-      @paren_count = old_paren_count
-
       false
-    ensure
-      check_close_paren
     end
 
     def visit(node : Union)
-      check_open_paren
+      write_token :OP_LPAREN if node.parens?
 
       if @token.type.ident? && @token.value == "self?" && node.types.size == 2 &&
          node.types[0].is_a?(Self) && node.types[1].to_s == "::Nil"
         write "self?"
         next_token
-        check_close_paren
+        write_token :OP_RPAREN if node.parens?
         return false
       end
 
@@ -1291,7 +1248,7 @@ module Crystal
         end
       end
 
-      check_close_paren
+      write_token :OP_RPAREN if node.parens?
 
       false
     end
@@ -2386,18 +2343,14 @@ module Crystal
     end
 
     def visit(node : ProcNotation)
-      check_open_paren
+      inputs = node.inputs
 
-      paren_count = @paren_count
+      has_input_parens = @token.type.op_lparen? &&
+                         !inputs.try(&.first?).try(&.location).try(&.equals?(@token.location))
 
-      if inputs = node.inputs
-        # Check if it's ((X, Y) -> Z)
-        #                ^    ^
-        sub_paren_count = @paren_count
-        if check_open_paren
-          sub_paren_count = @paren_count
-        end
+      write_token :op_lparen if has_input_parens
 
+      if inputs
         inputs.each_with_index do |input, i|
           accept input
 
@@ -2408,14 +2361,10 @@ module Crystal
           end
         end
 
-        if sub_paren_count != paren_count
-          check_close_paren
-        end
+        write_token :OP_RPAREN if has_input_parens
       end
 
-      skip_space_or_newline if paren_count == @paren_count
-      check_close_paren
-      skip_space
+      skip_space_or_newline
 
       write " " if inputs
       write_token :OP_MINUS_GT
@@ -2428,15 +2377,11 @@ module Crystal
         skip_space
       end
 
-      check_close_paren
-
       false
     end
 
     def visit(node : Self)
-      check_open_paren
       write_keyword :self
-      check_close_paren
       false
     end
 
@@ -4898,8 +4843,6 @@ module Crystal
     end
 
     def finish
-      raise "BUG: unclosed parenthesis" if @paren_count > 0
-
       skip_space
       write_line
       skip_space_or_newline last: true

spec/compiler/formatter/formatter_spec.cr
@@ -2097,10 +2097,10 @@ describe Crystal::Formatter do
     assert_format "G_(A, (B -> R))"
     assert_format "G_(A, ->)"
     assert_format "G_(A, (->))"
-    pending { assert_format "G_(A, () ->)" } # #16741
+    assert_format "G_(A, () ->)"
     assert_format "G_(A, -> R)"
     assert_format "G_(-> R)"
-    pending { assert_format "G_(() -> R)" } # #16741
+    assert_format "G_(() -> R)"
 
     assert_format "G_(A -> R | S)"
     assert_format "G_((A -> R | S))"
@@ -2111,6 +2111,15 @@ describe Crystal::Formatter do
     assert_format "G_(A | B -> R)"
     assert_format "G_((A | B) -> C)"
     assert_format "G_(A | (B -> C))"
+
+    assert_format "G_((A*) -> R)"
+
+    assert_format "G_(((A) ->) ->)"
+    assert_format "G_((A ->) ->)"
+    assert_format "G_(A -> ->)"
+    assert_format "G_((A -> ->))"
+    assert_format "G_(((A) ->, B) ->)"
+    assert_format "G_((A) | B ->)"
   end
 
   assert_format "foo &.bar.is_a?(Baz)"

spec/compiler/parser/inspect_spec.cr
@@ -239,8 +239,11 @@ describe "ASTNode#inspect" do
   expect_inspect %(foo : A | (B -> C)), <<-CRYSTAL
       TypeDeclaration[
         Var["foo"],
-        Union[Path["A"], ProcNotation[Path["B"], Path["C"]]]
+        Union[Path["A"], Union.parens(ProcNotation[Path["B"], Path["C"]])]
       ]
+      CRYSTAL
+  expect_inspect %(x : (A | B)), <<-CRYSTAL
+      TypeDeclaration[Var["x"], Union.parens(Path["A"], Path["B"])]
       CRYSTAL
   expect_inspect %(foo : Int32 = 1), <<-CRYSTAL
       TypeDeclaration[Var["foo"], Path["Int32"], value: NumberLiteral["1", :i32]]
@@ -340,7 +343,7 @@ describe "ASTNode#inspect" do
       Nop.new,
       block_arg: Arg[
         "block",
-        restriction: ProcNotation[Underscore.new, Underscore.new]
+        restriction: Union.parens(ProcNotation[Underscore.new, Underscore.new])
       ],
       block_arity: 1,
       double_splat: Arg["args"]
@@ -351,7 +354,7 @@ describe "ASTNode#inspect" do
       "foo",
       [],
       Nop.new,
-      block_arg: Arg["", restriction: ProcNotation[]],
+      block_arg: Arg["", restriction: Union.parens(ProcNotation[])],
       block_arity: 0
     ]
     CRYSTAL

spec/compiler/parser/parser_spec.cr
@@ -492,14 +492,19 @@ module Crystal
     it_parses "def foo(var : self.class); end", Def.new("foo", [Arg.new("var", restriction: Metaclass.new(Self.new))])
     it_parses "def foo(var : self*); end", Def.new("foo", [Arg.new("var", restriction: Self.new.pointer_of)])
     it_parses "def foo(var : Int | Double); end", Def.new("foo", [Arg.new("var", restriction: Crystal::Union.new(["Int".path, "Double".path] of ASTNode))])
+    it_parses "def foo(var : (Int | Double)); end", Def.new("foo", [Arg.new("var", restriction: Crystal::Union.parens(Crystal::Union.new(["Int".path, "Double".path] of ASTNode)))])
     it_parses "def foo(var : Int?); end", Def.new("foo", [Arg.new("var", restriction: Crystal::Union.new(["Int".path, "Nil".path(true)] of ASTNode))])
     it_parses "def foo(var : Int*); end", Def.new("foo", [Arg.new("var", restriction: "Int".path.pointer_of)])
     it_parses "def foo(var : Int**); end", Def.new("foo", [Arg.new("var", restriction: "Int".path.pointer_of.pointer_of)])
     it_parses "def foo(var : Int -> Double); end", Def.new("foo", [Arg.new("var", restriction: ProcNotation.new(["Int".path] of ASTNode, "Double".path))])
     it_parses "def foo(var : Int, Float -> Double); end", Def.new("foo", [Arg.new("var", restriction: ProcNotation.new(["Int".path, "Float".path] of ASTNode, "Double".path))])
-    it_parses "def foo(var : (Int, Float -> Double)); end", Def.new("foo", [Arg.new("var", restriction: ProcNotation.new(["Int".path, "Float".path] of ASTNode, "Double".path))])
+    it_parses "def foo(var : (Int, Float -> Double)); end", Def.new("foo", [Arg.new("var", restriction: Crystal::Union.parens(ProcNotation.new(["Int".path, "Float".path] of ASTNode, "Double".path)))])
     it_parses "def foo(var : (Int, Float) -> Double); end", Def.new("foo", [Arg.new("var", restriction: ProcNotation.new(["Int".path, "Float".path] of ASTNode, "Double".path))])
     it_parses "def foo(var : () -> Double); end", Def.new("foo", [Arg.new("var", restriction: ProcNotation.new([] of ASTNode, "Double".path))])
+    it_parses "x : (A -> B)", TypeDeclaration.new("x".var, declared_type: Crystal::Union.parens(ProcNotation.new(["A".path] of ASTNode, "B".path)))
+    it_parses "x : (A -> B).class", TypeDeclaration.new("x".var, declared_type: Metaclass.new(Crystal::Union.parens(ProcNotation.new(["A".path] of ASTNode, "B".path))))
+    it_parses "alias T = (A*) -> R", Alias.new("T".path, ProcNotation.new([Generic.new(Path.global("Pointer"), ["A".path] of ASTNode, suffix: :asterisk)] of ASTNode, "R".path))
+    it_parses "alias T = (A -> ) ->", Alias.new("T".path, ProcNotation.new([ProcNotation.new(["A".path] of ASTNode)] of ASTNode))
     it_parses "def foo(var : Char[256]); end", Def.new("foo", [Arg.new("var", restriction: "Char".static_array_of(256))])
     it_parses "def foo(var : Char[N]); end", Def.new("foo", [Arg.new("var", restriction: "Char".static_array_of("N".path))])
     it_parses "def foo(var : Int32 = 1); end", Def.new("foo", [Arg.new("var", 1.int32, "Int32".path)])
@@ -514,6 +519,7 @@ module Crystal
     it_parses "def foo(&\n); end", Def.new("foo", block_arg: Arg.new(""), block_arity: 0)
     it_parses "def foo(a, &block); end", Def.new("foo", [Arg.new("a")], block_arg: Arg.new("block"), block_arity: 0)
     it_parses "def foo(a, &block : Int -> Double); end", Def.new("foo", [Arg.new("a")], block_arg: Arg.new("block", restriction: ProcNotation.new(["Int".path] of ASTNode, "Double".path)), block_arity: 1)
+    it_parses "def foo(a, &block : ((Int -> Double))); end", Def.new("foo", [Arg.new("a")], block_arg: Arg.new("block", restriction: Union.parens(Union.parens(ProcNotation.new(["Int".path] of ASTNode, "Double".path)))), block_arity: 1)
     it_parses "def foo(a, & : Int -> Double); end", Def.new("foo", [Arg.new("a")], block_arg: Arg.new("", restriction: ProcNotation.new(["Int".path] of ASTNode, "Double".path)), block_arity: 1)
     it_parses "def foo(a, &block : Int, Float -> Double); end", Def.new("foo", [Arg.new("a")], block_arg: Arg.new("block", restriction: ProcNotation.new(["Int".path, "Float".path] of ASTNode, "Double".path)), block_arity: 2)
     it_parses "def foo(a, &block : Int, self -> Double); end", Def.new("foo", [Arg.new("a")], block_arg: Arg.new("block", restriction: ProcNotation.new(["Int".path, Self.new] of ASTNode, "Double".path)), block_arity: 2)
@@ -1255,7 +1261,8 @@ module Crystal
     it_parses "lib LibC\nfun getchar\nend", LibDef.new("LibC".path, [FunDef.new("getchar")] of ASTNode)
     it_parses "lib LibC\nfun getchar(...)\nend", LibDef.new("LibC".path, [FunDef.new("getchar", varargs: true)] of ASTNode)
     it_parses "lib LibC\nfun getchar : Int\nend", LibDef.new("LibC".path, [FunDef.new("getchar", return_type: "Int".path)] of ASTNode)
-    it_parses "lib LibC\nfun getchar : (->)?\nend", LibDef.new("LibC".path, [FunDef.new("getchar", return_type: Crystal::Union.new([ProcNotation.new, "Nil".path(true)] of ASTNode))] of ASTNode)
+    it_parses "lib LibC\nfun getchar : (->)?\nend", LibDef.new("LibC".path, [FunDef.new("getchar", return_type: Crystal::Union.new([Crystal::Union.parens(ProcNotation.new), "Nil".path(true)] of ASTNode))] of ASTNode)
+    it_parses "lib LibC\nfun getchar : ((->))?\nend", LibDef.new("LibC".path, [FunDef.new("getchar", return_type: Crystal::Union.new([Crystal::Union.parens(Crystal::Union.parens(ProcNotation.new)), "Nil".path(true)] of ASTNode))] of ASTNode)
     it_parses "lib LibC\nfun getchar(Int, Float)\nend", LibDef.new("LibC".path, [FunDef.new("getchar", [Arg.new("", restriction: "Int".path), Arg.new("", restriction: "Float".path)])] of ASTNode)
     it_parses "lib LibC\nfun getchar(a : Int, b : Float)\nend", LibDef.new("LibC".path, [FunDef.new("getchar", [Arg.new("a", restriction: "Int".path), Arg.new("b", restriction: "Float".path)])] of ASTNode)
     it_parses "lib LibC\nfun getchar(a : Int)\nend", LibDef.new("LibC".path, [FunDef.new("getchar", [Arg.new("a", restriction: "Int".path)])] of ASTNode)
@@ -3934,6 +3941,24 @@ module Crystal
       source = "@[::Foo]"
       node = Parser.parse(source).as(Annotation).path
       node_source(source, node).should eq("::Foo")
+    end
+
+    it "sets correct location of proc notation inputs" do
+      source = "alias T = ((A), (B)) -> R"
+      proc_notation = Parser.parse(source).as(Alias).value.should be_a(ProcNotation)
+      inputs = proc_notation.inputs.should be_a(Array(ASTNode))
+      path = inputs.first.should(be_a(Union)).types.first.should be_a(Path)
+      node_source(source, path).should eq "A"
+      node_source(source, inputs[1]).should eq "(B)"
+    end
+
+    it "sets correct location of proc notation inputs" do
+      source = "alias T = (A) -> R"
+      proc_notation = Parser.parse(source).as(Alias).value.should be_a(ProcNotation)
+      inputs = proc_notation.inputs.should be_a(Array(ASTNode))
+      path = inputs.first.should be_a(Path)
+      node_source(source, path).should eq "A"
+      node_source(source, proc_notation).should eq "(A) -> R"
     end
 
     it "sets args_in_brackets to false for `a.b`" do

spec/compiler/parser/to_s_spec.cr
@@ -124,9 +124,9 @@ describe "ASTNode#to_s" do
 
   # 14216
   expect_to_s "def foo(x, **args, &block : _ -> _)\nend"
-  expect_to_s "def foo(x, **args, &block : (_ -> _))\nend", "def foo(x, **args, &block : _ -> _)\nend"
+  expect_to_s "def foo(x, **args, &block : (_ -> _))\nend"
   expect_to_s "def foo(& : ->)\nend"
-  expect_to_s "def foo(& : (->))\nend", "def foo(& : ->)\nend"
+  expect_to_s "def foo(& : (->))\nend"
   expect_to_s "def foo(x : (T -> U) -> V, *args : (T -> U) -> V, y : (T -> U) -> V, **opts : (T -> U) -> V, & : (T -> U) -> V) : ((T -> U) -> V)\nend"
   expect_to_s "foo(x : (T -> U) -> V, W)"
   expect_to_s "foo[x : (T -> U) -> V, W]"

src/compiler/crystal/semantic/normalizer.cr
@@ -510,5 +510,14 @@ module Crystal
       values = [Var.new(var_name).at(expressions)] of ASTNode
       MultiAssign.new(targets, values).at(expressions)
     end
+
+    def transform(node : Union)
+      if node.singleton?
+        # If the union has just one type, return that instead of a union
+        node.types.first
+      else
+        super
+      end
+    end
   end
 end

src/compiler/crystal/syntax/ast.cr
@@ -2406,8 +2406,23 @@ module Crystal
 
   class Union < ASTNode
     property types : Array(ASTNode)
+    property? parens : Bool
 
-    def initialize(@types)
+    def self.parens(type : ASTNode)
+      # Wrap existing union in parens if it doesn't already have parens
+      if type.is_a?(Union) && !type.parens?
+        return type.tap { |t| t.parens = true }
+      end
+
+      new [type] of ASTNode, parens: true
+    end
+
+    def initialize(@types, @parens = false)
+    end
+
+    # A union with only one element typically represents parenthesis in the type grammar: `(A)`
+    def singleton?
+      types.size == 1
     end
 
     def accept_children(visitor)
@@ -2415,13 +2430,18 @@ module Crystal
     end
 
     def clone_without_location
-      Union.new(@types.clone)
+      Union.new(@types.clone, @parens)
     end
 
-    def_equals_and_hash types
+    def_equals_and_hash types, parens?
 
     def pretty_print(pp) : Nil
-      pp_type(pp, "Union[", "]") do
+      if parens?
+        before, after = "Union.parens(", ")"
+      else
+        before, after = "Union[", "]"
+      end
+      pp_type(pp, before, after) do
         pp_join(pp, types)
       end
     end

src/compiler/crystal/syntax/location.cr
@@ -98,6 +98,13 @@ class Crystal::Location
     end
   end
 
+  def equals?(other)
+    return false unless (@filename || "") == (other.filename || "")
+    return false unless @line_number == other.line_number
+    return false unless @column_number == other.column_number
+    true
+  end
+
   # Returns the number of lines between start and finish locations.
   def self.lines(start, finish)
     return unless start && finish && start.filename == finish.filename

commit 1a9e44591cbfc328819b27642b7be2d98f68038d

Reapply "Refactor `Formatter` code for simplification" (#16885) [fixup #16744] (#16908)
This reverts commit f331cb18deb7e4463cb3796cb944e7434698c3e2.

The original refactor in https://github.com/crystal-lang/crystal/pull/16745 broke an unexpected use case and was subsequently reverted in https://github.com/crystal-lang/crystal/pull/16885.
With https://github.com/crystal-lang/crystal/pull/16744 merged, we can reapply the patch because this use case now takes a different path.

This is actually necessary to fix a regression introduced in https://github.com/crystal-lang/crystal/pull/16744, resulting from a merge conflict with https://github.com/crystal-lang/crystal/pull/16885.

src/compiler/crystal/tools/formatter.cr
@@ -1215,37 +1215,21 @@ module Crystal
           break
         end
 
-        accept type
-
-        last = last?(i, node.types)
-        if last
-          skip_space
-        else
+        unless i == 0
           skip_space_or_newline
-        end
+          write_token " ", :op_bar, " "
+          skip_space
 
-        while true
-          case @token.type
-          when .op_bar?
-            write " | "
-            next_token_skip_space
-            if @token.type.newline?
-              write_line
-              write_indent(column)
-              next_token_skip_space_or_newline
-            end
-          when .op_rparen?
-            if @paren_count > 0
-              @paren_count -= 1
-              write ")"
-              next_token_skip_space
-            else
-              break
-            end
-          else
-            break
+          if @token.type.newline?
+            write_line
+            write_indent(column)
+            next_token_skip_space_or_newline
           end
         end
+
+        accept type
+
+        skip_space
       end
 
       write_token :OP_RPAREN if node.parens?